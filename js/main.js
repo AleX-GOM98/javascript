747a305a30687d51f5cc25d902e386cca98c3b08
Alex <lecoppc@gmail.com>
2020-10-15 21:33:21 -0300

    script java

---
+++ js/main.js
@@ -0,0 +1,52 @@
+/*
+var nome = "Alex Maia";
+var idade = 22;
+var idade2 = 10;
+//alert(nome + " tem " + idade + " anos. ");
+//alert(idade + idade2);
+console.log(nome.replace("Maia","Gonçalves"))
+*/
+/*
+var lista = ["maça", "pera", "laranja"];
+lista.push("Uva");
+lista.pop();
+console.log(lista);
+console.log(lista.toString());
+console.log(lista.join(" - "));
+*/
+/*
+var fruta = {nome: "maça", cor: "vermelha"}
+console.log(fruta);
+console.log(fruta.nome);
+*/
+/*
+var frutas = [{nome: "maça", cor: "vermelha"},{nome: "uva", cor: "roxa"}]
+console.log(frutas[1].nome);
+*/
+/*
+var idade = prompt("Qual sua idade");
+//var idade = 18;
+if (idade >= 18) {
+	alert("maior de idade");
+}else{
+	alert("menor de idade");
+}
+*/
+/*
+var count = 0;
+while(count <= 5){
+	console.log(count);
+	count++;
+};
+*/
+/*
+var count;
+for (var count = 0; count <= 5; count++) {
+	alert(count);
+};
+*/
+
+var d = new Date();
+alert(d.getMonth()+1);
+alert(d.getMinutes());
+alert(d.getHours());
